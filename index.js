64c61bf005b55abb4b9d2aa11b22f810c887a32d
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-16 21:20:26 -0600

    using routes for get members

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/Logger");
-const members = require("./Members");
 
 const app = express();
 
 //Init middleware
 //app.use(logger);
 
-//Gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
-//Get single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-
-  found
-    ? res.json(
-        members.filter((member) => member.id === parseInt(req.params.id))
-      )
-    : res
-        .status(400)
-        .json({ msg: `No member with the id of ${req.params.id}` });
-});
-
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/api/members.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const members = require("../../Members");
+
+//Gets all members
+router.get("/", (req, res) => res.json(members));
+
+//Get single member
+router.get("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  found
+    ? res.json(
+        members.filter((member) => member.id === parseInt(req.params.id))
+      )
+    : res
+        .status(400)
+        .json({ msg: `No member with the id of ${req.params.id}` });
+});
+
+module.exports = router;
